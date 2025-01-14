5f4fe9c46312d8dad6c48692b92b4df268a1d581
stephenHe <hf597833184@163.com>
2025-01-14 21:27:40 +0800

    feat: add tailwind

---
+++ src/editor/index.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: stephenHe
+ * @Date: 2025-01-14 21:20:21
+ * @LastEditors: stephenHe
+ * @LastEditTime: 2025-01-14 21:20:27
+ * @Description:
+ * @FilePath: /lowcode-editor/src/editor/index.tsx
+ */
+export default function LowCodeEditor() {
+  return <div>LowCodeEditor</div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: stephenHe
+ * @Date: 2025-01-14 21:22:28
+ * @LastEditors: stephenHe
+ * @LastEditTime: 2025-01-14 21:23:12
+ * @Description: tailwind.config.js
+ * @FilePath: /lowcode-editor/tailwind.config.js
+ */
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
